5951313be5a95bb09ab027a045db09fac8b542b2
Thomas Eriksson <thomas.eriksson@gmail.com>
2010-03-07 12:47:09 +0100

    amigados theme, with guru meditation :D

---
+++ data/themes/computer/amigados/prompt.bash
@@ -0,0 +1,35 @@
+#!/bin/sh
+## @(#) created by arne
+#######################
+## theme: amiga
+##
+## the amiga CLI prompt
+## the title and the prompt is 100% correct as of the kickstart 1.3 rom
+## seen in the UAE emulator
+
+_bashish_guru()
+{
+	typeset RETVALUE="$?"
+	test x$RETVALUE = x0 && return 0
+	test x"$?" = x255 && return "$RETVALUE"
+	test $RETVALUE -lt 136 && return "${RETVALUE}"
+	clear
+	printf "\033[40m"
+	typeset BASHISH_ASCIILOGO=""
+	BASHISH_ASCIILOGOG= _bashish_prompt_asciilogo logo.guru 32
+	read -n1
+	printf "\033[0m"
+	clear
+	return ${RETVALUE}
+}
+_bashish_prompt()
+{
+eval $(_bashish_prompt_shellvars $SHELLNAME)
+PROMPT_COMMAND="_bashish_guru"
+_bashish_prompt_asciilogo nocenter logo.start
+test "x${PROMPTSTR}" = x && $_typeset PROMPTSTR="${TTY##*/}"
+test "x${TITLESTR}" = x && $_typeset TITLESTR="AmigaDOS"
+
+PS1="${PROMPTSTR}>"
+TITLE="${TITLESTR}"
+}
