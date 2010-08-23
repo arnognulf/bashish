a9a5a67d93870c1ec02d112e4192057157a53ee8
Thomas Eriksson <thomas.eriksson@gmail.com>
2010-08-23 17:57:16 +0200

    more foobars work

--- data/themes/ascii-art/foobars/prompt.sh
+++ data/themes/ascii-art/foobars/prompt.sh
@@ -23,13 +23,12 @@ _bashish_prompt ()
 {
 eval $(_bashish_prompt_shellvars $SHELLNAME)
 eval $(_bashish_prompt_parsecolors "$@")
-test "x${BASHISH_COLOR0}" = x && eval $(_bashish_prompt_parsecolors green red white)
+test "x${BASHISH_COLOR0}" = x && eval $(_bashish_prompt_parsecolors red green blue)
 test "x${BASHISH_COLOR1}" = x && eval $(_bashish_prompt_parsecolors $1 red white)
 test "x${BASHISH_COLOR2}" = x && eval $(_bashish_prompt_parsecolors $1 $2 white)
 
 local i=0
 local FILLY=""
-local BASHISH_CWD=`_bashish_prompt_cwd "${SHELLNAME}" "${ESC}[3${BASHISH_COLOR1}m" "${ESC}[3${BASHISH_COLOR2}m" 39 embedall`
 PROMPT_COMMAND="_BASHISH_PROMPT_RCS=\$(_bashish_prompt_rcs \${_BASHISH_PROMPT_RCS} ) && _bashish_promptupdate"
 
 local RCS_PS1=$(_bashish_prompt_rcs)
@@ -39,44 +38,49 @@ test "x$PROMPTSTR" = x && local PROMPTSTR="${USER}${ESC}[4${BASHISH_COLOR0};3${B
 
 local ROOT="="
 test "x${UID}" = x0 && ROOT="#"
-
+local CWD_SEPCOLOR="${ESC}[${INVERSE};${BOLD};4${BASHISH_COLOR1}${SETCOLOR}"
+local CWD_TXTCOLOR="${ESC}[0;${INVERSE}${SETCOLOR}"
+local CWD_STR=$(_bashish_prompt_cwd ${SHELLNAME} "${CWD_SEPCOLOR}" "${CWD_TXTCOLOR}" 42 embedall)
 
 PS1="${EMBED}\
 ${ESC}[2A\
 ${ESC}[2B\
-${ESC}[0m\
+${ESC}[${NORMAL}${SETCOLOR}\
 ${ESC}7\
-${ESC}[4${BASHISH_COLOR0};30m\
+${ESC}[0m\
 ${ESC}[0;$(($LINES - 1))r\
 ${ESC}[$LINES;0H\
 ${ESC}[K\
-${ESC}[0m\
-${ESC}[K\
-${ESC}[4${BASHISH_COLOR0};30m  \
-${ESC}[E\
-${ESC}[$(($LINES));0H\
-${ESC}[0;3${BASHISH_COLOR0}m`_bashish_prompt_cp437 DF`\
-${ESC}[4${BASHISH_COLOR0};30m`_bashish_prompt_fillx \" \" 2`\
-${ESC}[0;3${BASHISH_COLOR0}m`_bashish_prompt_cp437 DC`\
-${ESC}[$LINES;4H\
-${ESC}[3${BASHISH_COLOR2}m$PROMPTSTR\
-${ESC}[2C\
-${ESC}[3${BASHISH_COLOR2}m${UNEMBED}$BASHISH_CWD${EMBED}${RCS_PS1}\
-${ESC}[33m\
-${ESC}[$LINES;$(($COLUMNS - 12))H\
-${ESC}[3${BASHISH_COLOR2}m${TIME}\
-${ESC}[3${BASHISH_COLOR1}m\
-${ESC}[6D:\
-${ESC}[2C:\
-${ESC}[2C\
-${ESC}[3${BASHISH_COLOR1}m)\
-${ESC}8\
-${ESC}[0;4;3${BASHISH_COLOR0}m\
-${ESC}[0;4${BASHISH_COLOR0};3${BASHISH_COLOR1}m\
-${ESC}[0;3${BASHISH_COLOR1};${INVERSE}m\
-${UNEMBED}>\
+${ESC}[0;4${BASHISH_COLOR0}m\
+     \
+${ESC}[0;${INVERSE}${SETCOLOR}\
+ ${USER}\
+${ESC}[4${BASHISH_COLOR0}${SETCOLOR}\
+@\
+${ESC}[0;${INVERSE}${SETCOLOR}\
+${HOSTNAME%%.*} \
+${ESC}[${NORMAL}${SETCOLOR}\
+ \
+${ESC}[0;3${WHITE};4${BASHISH_COLOR1}${SETCOLOR}\
+     \
+${ESC}[0;${INVERSE}${SETCOLOR}\
+${UNEMBED}\
+ ${CWD_STR} \
 ${EMBED}\
-${ESC}[0m\
+${ESC}[${NORMAL}${SETCOLOR}\
+ \
+${ESC}[0;4${BASHISH_COLOR2}m\
+     \
+${ESC}[0;${INVERSE}${SETCOLOR}\
+ baz \
+${ESC}[${NORMAL}${SETCOLOR}\
+ \
+${ESC}8\
+${ESC}[${NORMAL};${INVERSE}m\
+${UNEMBED}\
+ \$ \
+${EMBED}\
+${ESC}[${NORMAL}m\
 ${UNEMBED}\
  "
 }
